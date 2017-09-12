27c76fb830839fc22999d3fa3e9e334eb3ac8521
jillco <jillcarranza@gmail.com>
2017-09-12 14:12:16 -0600

    ejemplo de multiplicacion de registros

---
+++ multiplicacion.asm
@@ -0,0 +1,39 @@
+section .data
+nombre_archivo: dw '/sys/class/block/sda/size'
+tam_nombre_archivo: equ $-nombre_archivo
+
+section .bss
+contenido_archivo: resb 10
+y: resb 100
+section .text
+	global _start
+
+_start:
+	
+	mov ebx,nombre_archivo 
+	mov eax,5 ; utilizado para abrir el archivo, se hace llamada al sistema eax=5
+	mov ecx,0
+	;mov edx,y
+	int 80h
+
+	mov eax,3 ; se lee el archivo
+	mov ebx,eax
+	mov ecx,contenido_archivo
+	mov edx,y
+	int 80h
+	
+	mov eax,4  ; se escribe en pantallla
+	mov ebx,1
+	mov ecx,contenido_archivo
+	;mov edx,y
+	int 80h
+	
+	mov eax,200h  ;multiplicacion de dos números en hexadecimal
+	mov ebx,56h
+	mul ebx
+	int 80h
+	
+	break1:
+	mov eax,1
+	mov ebx,0
+	int 0x80
